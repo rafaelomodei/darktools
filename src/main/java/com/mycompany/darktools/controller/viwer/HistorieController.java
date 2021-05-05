7cd585e6f873745e4ea540dc0eb58fbae1820799
Rafael Omodei <omodei@alunos.utfpr.edu.br>
2021-05-05 00:36:22 -0300

    adicionando opcao de botoes na historia

--- src/main/java/com/mycompany/darktools/controller/viwer/HistorieController.java
+++ src/main/java/com/mycompany/darktools/controller/viwer/HistorieController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
@@ -34,7 +35,7 @@ public class HistorieController implements Initializable {
     private Label label_titleHistorie;
 
     @FXML
-    private ImageView imageView_charater;
+    private ImageView imageView_character;
 
     @FXML
     private Label label_descripitionHistorie;
@@ -42,25 +43,29 @@ public class HistorieController implements Initializable {
     @FXML
     private Button button_next;
     
-    private String SCENE_URL =  getClass().getResource("/iu/img/image_02.png").toString();
+    private String SCENE_URL =  getClass().getResource("/iu/img/image_03.png").toString();
     private String CHARATER_URL =  getClass().getResource("/iu/img/acher_self.png").toString();
-
+    private Image imgScene;
+    private Image imgCharacter;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        imageView_scene = new ImageView(SCENE_URL);
-        imageView_scene.setPreserveRatio(true);
-        imageView_scene.setFitWidth(1280);
-        imageView_scene.setX(152.0);
-        borderPane_scene.setCenter(imageView_scene);
+        imgScene = new Image(SCENE_URL);
+                
+        imageView_scene.setImage(imgScene);
         
-        imageView_charater = new ImageView(CHARATER_URL);
-        imageView_charater.setPreserveRatio(true);
-        imageView_charater.setFitWidth(100);
-        imageView_charater.setX(100);
-        borderPane_historie.setCenter(imageView_charater);
+        label_titleHistorie.setText("Dive Rock");
+        
+        
+        imgCharacter = new Image(CHARATER_URL);
+        imageView_character.setImage(imgCharacter);
+//        imageView_charater = new ImageView(CHARATER_URL);
+//        imageView_charater.setPreserveRatio(true);
+//        imageView_charater.setFitWidth(100);
+//        imageView_charater.setX(100);
+        //borderPane_historie.setCenter(label_titleHistorie);
         
     }    
     
