29addf4716b61deb7cc4572acff98110e5c8a9a5
Rafael Omodei <omodei@alunos.utfpr.edu.br>
2021-05-20 01:44:38 -0300

    Criando tela de creditos

---
+++ src/main/java/com/mycompany/darktools/controller/viwer/CreditsControllerViwer.java
@@ -0,0 +1,45 @@
+package com.mycompany.darktools.controller.viwer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mycompany.darktools.controller.ViwerController;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rafae
+ */
+public class CreditsControllerViwer implements Initializable {
+
+    @FXML
+    private Label label_credits;
+
+    @FXML
+    private Button button_back;
+
+    @FXML
+    private Label label_nameGame;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+    
+        @FXML
+    private void switchToWindow() throws IOException {
+        ViwerController viwerController = ViwerController.getStante();
+        viwerController.setRoot("Home");
+    }
+    
+}
